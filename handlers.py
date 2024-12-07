a90ade294763a908525b21e511007ea4bdf7a982
AlinaVoronina876 <alya.voronina.0600@mail.ru>
2024-12-07 18:51:53 +0300

    внесла изменения в handlers

--- handlers.py
+++ handlers.py
@@ -5,17 +5,3 @@ def get_request(url):
     responses = requests.get(url)
     return responses.text
 
-def parse_iso_table(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    klass = soup.find_all('table')[1].find_all('tr')
-
-    currencies = []
-    for row in klass:
-        if row.find_all('td') == []:
-            continue
-        code = row.find_all('td')[0].text
-        num = row.find_all('td')[1].text
-        currency = row.find_all('td')[3].text
-        currencies.append((code, num, currency))
-
-    return currencies
