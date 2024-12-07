ec5095b661b6aa69dd3c9878f3452e7f0f3384bf
AlinaVoronina876 <alya.voronina.0600@mail.ru>
2024-12-07 19:22:53 +0300

    внесла изменения в main

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from handlers import get_request, parse_iso_table
+from handlers import get_request
 
 url = 'https://en.wikipedia.org/wiki/ISO_4217'
 
 def main():
     print('Code  ', 'Num   ', 'Currency   ')
     text = get_request(url)
-    currencies = parse_iso_table(text)
-
-    for code, num, currency in currencies:
-        print(f'{code}   {num}   {currency}')
 
 if __name__ == "__main__":
     main()
